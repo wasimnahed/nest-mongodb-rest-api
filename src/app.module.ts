43aca2dc215ed7b29b752bece6eb62806f9cdf40
wasimnahed <Wasim.Nahed@softobiz.com>
2022-09-02 22:42:47 +0530

    db with nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [".local.env"]
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({ uri: configService.get("MONGO_URI") }),
+      inject: [ConfigService],
+    }),
+    BookModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
